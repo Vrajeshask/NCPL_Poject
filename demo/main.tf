7b26c12ebfa4cfdbe02a21926cc59f378e5b6992
Vrajesh <patelvrajeshjagdishbhai@gmail.com>
2024-02-23 16:06:44 +0000

    Updated main.tf

--- demo/main.tf
+++ demo/main.tf
@@ -2,7 +2,18 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "ncpl-demo" {
-  name     = "ncpl-demo"
+resource "azurerm_resource_group" "ncpl-rg" {
+  name     = "ncpl-rg"
   location = "eastus"
+}
+
+resource "azurerm_container_registry" "ncplcr" {
+  name                     = "ncplcr "
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  sku                      = "Basic"  # Change this to the desired SKU (e.g., "Basic", "Standard", "Premium")
+  admin_enabled            = true     # Set to true if you want to enable admin access to the registry
+    
+    
+  }
 }
