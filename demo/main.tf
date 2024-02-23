315d0a6bdbc8923d8d116e98babc036e7d62a384
Vrajesh <patelvrajeshjagdishbhai@gmail.com>
2024-02-23 16:08:35 +0000

    Updated main.tf

--- demo/main.tf
+++ demo/main.tf
@@ -7,13 +7,31 @@ resource "azurerm_resource_group" "ncpl-rg" {
   location = "eastus"
 }
 
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}
+
 resource "azurerm_container_registry" "ncplcr" {
-  name                     = "ncplcr "
+  name                     = "ncplcr"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   sku                      = "Basic"  # Change this to the desired SKU (e.g., "Basic", "Standard", "Premium")
   admin_enabled            = true     # Set to true if you want to enable admin access to the registry
+
+  # Network rule to allow access only from specific IP addresses or ranges
+  network_rule_set {
+    default_action = "Deny"
     
-    
+    ip_rule {
+      action = "Allow"
+      ip_range = "YOUR_PUBLIC_IP_OR_RANGE"  # Replace with your IP address or range
+    }
   }
+}
+
 }
